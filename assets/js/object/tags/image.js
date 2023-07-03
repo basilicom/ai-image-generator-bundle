c4ffeeb8f2f7e2511b218baaa59a839942f36d21
alexander.heidrich <alexander.heidrich@basilicom.de>
2023-07-03 17:56:33 +0200

    Split JS and CSS paths into editmode and backend

--- assets/js/object/tags/image.js
+++ assets/js/object/tags/image.js
@@ -1,4 +1,17 @@
 pimcore.registerNS("pimcore.object.tags.image");
 pimcore.object.tags.image = Class.create(pimcore.object.tags.image, {
+    type: "image",
+    dirty: false,
 
+    initialize: function (data, fieldConfig) {
+        if (data) {
+            this.data = data;
+        } else {
+            this.data = {};
+        }
+
+        this.fieldConfig = fieldConfig;
+
+        console.log("whuhuhU");
+    }
 });
